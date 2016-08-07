3701c6efb97bbc985680e496c72d72579d28a3aa
sadaust <maperaust@yahoo.com>
2016-08-06 19:58:57 -0700

    Basic Idea for characters
    This is my thoughts on what the Trail part of the game might need from
    the characters

---
+++ E3Trail/E3Trail/Character.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+
+class Character {
+private:
+	int hp;
+	int resource;
+	float resrate;
+	std::string name;
+	std::string resName;
+public:
+	void init(std::string a_name, std::string resourceName) {
+		hp = 100;
+		resource = 0;
+		name = a_name;
+		resName = resourceName;
+	}
+	int getHP() {return hp;}
+	int resource() {return resource;}
+	std::string getResName() {return resName;}
+	std::string getName() {return name;}
+	void resIncer() {resource += resrate*hp;}
+	void modHp(int mod) {hp+= mod;}
+	void modResource(int mod) {resource+=mod;if(resource<0){resource = 0;}}
+};
--- E3Trail/E3Trail/Trail.h
+++ E3Trail/E3Trail/Trail.h
@@ -1,3 +1,16 @@
 #pragma once
 #include "Engine.h"
+#include "Character.h"
 
+
+#define PARTYSIZE 4
+
+class Trail {
+private:
+	int food;
+	int credits;
+	int fuel;
+	Character party[PARTYSIZE];
+public:
+	
+};
